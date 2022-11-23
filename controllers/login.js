acc2139376b55b9ed56cae9303464677e4e4c2f5
Joshua Peoples <jpcoder12@gmail.com>
2022-11-23 15:48:24 -0700

    map items test

--- controllers/login.js
+++ controllers/login.js
@@ -10,14 +10,20 @@ router.get('/', (req, res, next) => {
 })
 
 
-
+let items = [
+    {name: 'item1', price: '100'},
+    {name: 'item2', price: '150'}
+]
 router.post('/POSTDATA', (req, res, next) => {
     if (req.body){
         console.log(req.body)
     } else {
         res.send('There was no data')
     }
-    res.render('dashboard', {title: 'Dashboard'})
+    res.render('dashboard', {
+        title: 'Dashboard', 
+        items
+    })
 })
 
 module.exports = router
