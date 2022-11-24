cf079c829cc9eb5e782cb6d26b2deccd5938558d
Joshua Peoples <jpcoder12@gmail.com>
2022-11-24 10:19:11 -0700

    create user page front end works

--- public/formfactor.js
+++ public/formfactor.js
@@ -1,3 +1,16 @@
 const changeLabel = () => {
     document.querySelector("#js-test").innerHTML = document.querySelector("#js-input").value
 }
+
+const adminControlKey = () => {
+    let option_val = document.querySelector("#select-type").value
+    if (option_val == 'Owner'){
+        document.querySelector('#admin-key').style.display = "inline"
+        document.querySelector('#admin-key').required = true;
+    }
+    else {
+        document.querySelector('#admin-key').style.display = "none"
+        document.querySelector('#admin-key').removeAttribute('required')
+
+    }
+}
