7406cdd86472332c98777ef56598c5091ee5097d
Joshua Peoples <jpcoder12@gmail.com>
2022-11-24 21:05:03 -0700

    remove login.js and replace on server.js
    Also adjust routes and form posts

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const Users = require('../models/users')
 
-router.get('/', (req, res, next) => {
-    res.render('dashboard')
+router.get('/', async (req, res, next) => {
+    // In order to access this page, the user must be authenticated. That user's data will be passed to the template. 
+    const user = await Users.findOne({})
+    console.log('User info below: \n', user)
+    res.render('dashboard', {user: user, title: 'Dashboard', data: [{name: 'Pepperoni', type: 'Meat', price: '2.00', img: './images/pepperoni.png', createdby: user.username}]})
 })
 
 
--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const express = require('express')
 const app = express()
 const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
+const Users = require('./models/users')
+const bcrypt = require('bcrypt')
 require('dotenv').config()
+
+// Access forms inside of req
+app.use(express.urlencoded({extended: false}))
 
 // MongoDB
 mongoose.connect(
@@ -23,18 +28,47 @@ app.use(express.static("public"))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
+
 // Routes
 // This is temporary, when I add passport this will be checking for authentication before going to login
-app.use('/', require('./controllers/login'))
 app.use('/dashboard', require('./controllers/dashboard'))
 
+app.get('/', (req, res, next) => {
+    // if user is authenticated, render dashboard with data. Otherwise, show login. 
+    res.render('login')
+})
+app.get('/new', (req, res, next) => {
+    res.render('newuser')
+})
+app.post('/new', async (req, res, next) => {
+    try {
+        const hashPass = await bcrypt.hash(req.body.password, 10)
+        Users.create({
+            username: req.body.username,
+            fname: req.body.fname,
+            lname: req.body.lname || '',
+            password: hashPass,
+            permissions: req.body.permissions
+        })
+        console.log('User successfully created')
+        res.redirect('/')
+    } catch {
+        console.log('error')
+    }
+})
+app.post('/login-user', async (req, res, next) => {
+    
+    console.log(req.body.username)
+    
+    res.redirect(`dashboard`)
+    
+})
 app.get('/index', (req, res, next) => {
     res.render('index')
-    console.log('hooray the server works')
 })
 app.get('/test', (req, res, next) => {
     res.render('visualtest')
-    console.log('Test Mode')
+    console.log('Test styles')
 })
 
 
