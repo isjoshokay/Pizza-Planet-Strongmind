dbfef4e924fd320005778c96359ee9437e1109fa
Joshua Peoples <jpcoder12@gmail.com>
2022-11-27 14:56:26 -0700

    toggle selection of toppings and add to list

--- public/formfactor.js
+++ public/formfactor.js
@@ -23,12 +23,39 @@ const displayModal = (e) => {
     document.querySelector("#update-topping-id").value = e.children[1].innerHTML
     document.querySelector(".modal-container").style.display = "inline"
 }
+//This closes both modals on either 'dashboard' page
+const closeModal = () => {
+    document.querySelector(".modal-container").style.display = "none"
+}
 
 // This is for editing a pizza only
+let toppingsList = [] // This will be populated with all of the toppings the pizza has by name
+// the array will be appended to a hidden input for the form. 
 const displayPizzaModal = (e) => {
 
 }
 
+// This is for adding and removing toppings from a pizza. (see above toppingsList var for more details)
+let newToppingsList = [] 
+const addOrRemoveTopping = e => {
+    //if it's selected, remove the topping name from newToppingsList and remove the 'selected' class
+    if (e.classList.contains("selected")){
+        e.classList.toggle("selected")
+        let itemIdx = newToppingsList.indexOf(e.children[0].value)
+        if (itemIdx !== -1) {
+            newToppingsList.splice(itemIdx, 1);
+        }
+        console.log(newToppingsList)
+        console.log(e.children[0].value, 'was removed from the pizza')
+    } else {
+        e.classList.toggle("selected")
+        newToppingsList.push(e.children[0].value)
+        console.log(newToppingsList)
+        console.log(e.children[0].value, 'was added to the pizza')
+    }
+}
+
+// This is for creating a new pizza 
 const displayNewPizza = () => {
     // Set the other two displays to none and display Create Pizza view. 
     document.querySelector("#pizza-update-wrapper").style.display = "none"
@@ -36,7 +63,3 @@ const displayNewPizza = () => {
     document.querySelector("#pizza-new-wrapper").style.display = "inline" 
 }
 
-//This closes both modals on either 'dashboard' page
-const closeModal = () => {
-    document.querySelector(".modal-container").style.display = "none"
-}
