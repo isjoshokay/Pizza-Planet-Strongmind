b82139af16fbb84e9cf7022eefca0fc0d9027432
Joshua Peoples <jpcoder12@gmail.com>
2022-11-29 17:52:40 -0700

    add ability to add topping to update pizza

--- public/formfactor.js
+++ public/formfactor.js
@@ -73,34 +73,35 @@ const populateToppings = e => {
 // This is for adding and removing toppings from a *new* pizza.
 let newToppingsList = [] 
 const addOrRemoveTopping = e => {
+    // When updating a pizza
     if (e.classList.contains("available-topping-update")){
-        console.log(toppingsToUpdate)
         //if it's selected, remove the topping name from toppingsToUpdate and remove the 'selected' class
         if (e.classList.contains("selected")){
-            e.classList.toggle("selected")
             let itemIdx = toppingsToUpdate.indexOf(e.children[0].value)
             if (itemIdx !== -1) {
                 toppingsToUpdate.splice(itemIdx, 1);
             }
             document.querySelector("#update-toppings-on-pizza").value = toppingsToUpdate
         } else {
-
+            toppingsToUpdate.push(e.children[0].value)
+            document.querySelector("#update-toppings-on-pizza").value = toppingsToUpdate
         }
+    // When creating a new pizza
     } else if (e.classList.contains("available-topping")) {
         //if it's selected, remove the topping name from newToppingsList and remove the 'selected' class
         if (e.classList.contains("selected")){
-            e.classList.toggle("selected")
             let itemIdx = newToppingsList.indexOf(e.children[0].value) // the topping name
             if (itemIdx !== -1) {
                 newToppingsList.splice(itemIdx, 1);
             }
             document.querySelector("#new-toppings-on-pizza").value = newToppingsList
         } else {
-            e.classList.toggle("selected")
+            
             newToppingsList.push(e.children[0].value)
             document.querySelector("#new-toppings-on-pizza").value = newToppingsList
         }
     }
+    e.classList.toggle("selected")
 }
 // This is for creating a new pizza 
 const displayNewPizza = () => {
